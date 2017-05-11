d55568f56bddbc16ce1e7d06d39915bb1dedc447
awells111 <awells111@gmail.com>
2017-05-11 17:10:54 -0400

    Choose Suspect and Send Crime Report Buttons implemented

--- app/src/main/java/com/wellsandwhistles/crimewatch/CrimeLab.java
+++ app/src/main/java/com/wellsandwhistles/crimewatch/CrimeLab.java
@@ -104,7 +104,8 @@ public class CrimeLab {
         values.put(CrimeTable.Cols.UUID, crime.getId().toString());
         values.put(CrimeTable.Cols.TITLE, crime.getTitle());
         values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
-        values.put(CrimeTable.Cols.SOLVED, crime.isSolved());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        values.put(CrimeTable.Cols.SUSPECT, crime.getSuspect());
 
         return values;
     }
